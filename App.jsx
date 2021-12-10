b3b664c32ce5062cf8c793e62cd85ffada2ffaee
chihiroIshizaki <95729805+chihiroIshizaki@users.noreply.github.com>
2021-12-10 16:07:04 +0900

    create memoeditscreen and memocreate screen

--- App.jsx
+++ App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
-import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 export default function App() {
   return (
     // <MemoDetailScreen />
-    <MemoListScreen />
+    // <MemoEditScreen />
+    // <MemoListScreen />
+    <MemoCreateScreen />
   );
 }
